af4a6991b4c910335fae976c9d61379f9af721c3
Jo Minsu <cominswu@gmail.com>
2025-09-23 23:41:50 +0900

    feat: ch03 2.단항연산자

---
+++ src/ch03/OperatorEx.java
@@ -0,0 +1,14 @@
+package ch03;
+
+class OperatorEx {
+    public static void main(String[] args) {
+        int i = 5;
+        i++;
+
+        System.out.println(i);
+
+        i=5;
+        ++i;
+        System.out.println(i);
+    }
+}
