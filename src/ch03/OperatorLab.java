e25a26f8328fdc03344ba87a44743026b45e074b
Jo Minsu <cominswu@gmail.com>
2025-09-25 12:06:58 +0900

    feat: Add examples for arithmetic operators

--- src/ch03/OperatorLab.java
+++ src/ch03/OperatorLab.java
@@ -1,6 +1,6 @@
 package ch03;
 
-class OperatorLab {
+/*class OperatorLab {
     public static void main(String[] args) {
         int i = 5;
         int j = 0;
@@ -12,5 +12,20 @@ class OperatorLab {
 
         System.out.println("최종 결과: i = " + i + ", j = " + j);
         //실행결과는 최종 결과: i = 7, j = 12 이다
+    }
+}*/
+
+class OperatorLab {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10.0f;
+
+        // 1번 문제
+        System.out.println(a == b);
+
+        // 2번 문제
+        double d = 0.1;
+        float f = 0.1f;
+        System.out.println(d == f);
     }
 }
