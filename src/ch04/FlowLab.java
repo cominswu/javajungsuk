515dd6bac54138c2e713db96726cf4c41cfd4436
jominsu <cominswu@gmail.com>
2025-10-07 11:45:38 +0900

    feat: add examples for conditional statements

---
+++ src/ch04/FlowLab.java
@@ -0,0 +1,11 @@
+package ch04;
+import java.util.*;
+
+class FlowLab {
+    //Quiz: for문을 사용하여 "환영합니다!" 라는 메시지를 5번 출력하는 코드를 작성해보세요.
+    public static void main(String[] args) {
+        for(int i = 0; i < 5 ; i++) {
+            System.out.println("환영합니다!");
+        }
+    }
+}
