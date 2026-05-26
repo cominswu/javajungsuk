a4576303ed13bb12b68dd85c971d5747914739da
202445061 <cominswu@gmail.com>
2026-05-26 20:42:54 +0900

    feat:ch06 5.생성자

---
+++ src/ch06/CarEx3.java
@@ -0,0 +1,36 @@
+package ch06;
+
+class Car2 {
+    String color;
+    String gearType;
+    int door;
+
+    Car2() {
+        this("white", "auto", 4);
+    }
+    Car2(Car2 c) {
+        color = c.color;
+        gearType = c.gearType;
+        door = c.door;
+    }
+    Car2(String color, String gearType, int door) {
+        this.color = color;
+        this.gearType = gearType;
+        this.door = door;
+    }
+}
+
+public class CarEx3 {
+    public static void main(String[] args) {
+        Car2 c1 = new Car2();
+        Car2 c2 = new Car2(c1);
+
+        System.out.println("c1 의 color = " + c1.color + ", gearType = " + c1.gearType + ", door = " + c1.door);
+        System.out.println("c2 의 color = " + c2.color + ", gearType = " + c2.gearType + ", door = " + c2.door);
+
+        c1.door = 100;
+        System.out.println("c1.door = 100; 수행 후");
+        System.out.println("c1 의 color = " + c1.color + ", gearType = " + c1.gearType + ", door = " + c1.door);
+        System.out.println("c2 의 color = " + c2.color + ", gearType = " + c2.gearType + ", door = " + c2.door);
+    }
+}
