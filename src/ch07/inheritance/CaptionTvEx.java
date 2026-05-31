405d12dec037929079561aea982b5b132fa07805
202445061 <cominswu@gmail.com>
2026-05-31 17:59:49 +0900

    feat:ch07 1.상속

---
+++ src/ch07/inheritance/CaptionTvEx.java
@@ -0,0 +1,30 @@
+package ch07.inheritance;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
+}
+
+class CaptionTv extends Tv {
+    boolean caption;   //자막
+    void displayCaption (String text) {
+        if (caption)   //자막 상태가 true 일때 출력
+            System.out.println(text);
+    }
+}
+
+public class CaptionTvEx {
+    public static void main(String[] args) {
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10;
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+        ctv.displayCaption("Hello, World");
+        ctv.caption = true;
+        ctv.displayCaption("Hello, World");
+    }
+}
