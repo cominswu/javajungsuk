f34dd37a3abac39bb70dcefdfd3fd972a3278e04
202445061 <cominswu@gmail.com>
2026-06-07 23:11:11 +0900

    feat:ch07 3.패키지와 임포트

---
+++ src/ch07/패키지와_임포트/PackageTest.java
@@ -0,0 +1,10 @@
+// package com.codechobo.book;
+// 자바의 정석 361~363 페이지
+package ch07.패키지와_임포트;
+
+
+public class PackageTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
