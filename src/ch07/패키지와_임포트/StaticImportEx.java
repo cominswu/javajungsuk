a374b7e340d7275f49021b1d6adc041a7008889c
202445061 <cominswu@gmail.com>
2026-06-08 22:24:19 +0900

    feat:ch07 3.패키지와 임포트

---
+++ src/ch07/패키지와_임포트/StaticImportEx.java
@@ -0,0 +1,16 @@
+package ch07.패키지와_임포트;
+
+import java.lang.Math;
+
+import static java.lang.Math.*;
+import static java.lang.System.out;
+
+class StaticImportEx {
+    public static void main(String[] args) {
+        //System.out.println(Math.random());
+        out.println(random());
+
+        //System.out.println("Math.PI : " + Math.PI);
+        out.println("Math.PI : " + PI);
+    }
+}
