48ee83f4662a6079377c6374735a1502c4b4c422
202445061 <cominswu@gmail.com>
2026-06-29 23:01:17 +0900

    feat:ch07 7.인터페이스

--- src/ch07/interfaceclass/FighterEx.java
+++ src/ch07/interfaceclass/FighterEx.java
@@ -4,8 +4,8 @@ class FighterEx {
     public static void main(String[] args) {
         Fighter f = new Fighter();
 
-        if (f instanceof Unit)
-            System.out.println("f는 Unit클래스의 자손입니다.");
+        if (f instanceof Unit1)
+            System.out.println("f는 Unit1클래스의 자손입니다.");
 
         if (f instanceof Fightable)
             System.out.println("f는 Fightable인터페이스를 구현했습니다.");
@@ -21,12 +21,12 @@ class FighterEx {
     }
 }
 
-class Fighter extends Unit implements Fightable {
+class Fighter extends Unit1 implements Fightable {
     public void move(int x, int y) { /* 내용 생략 */ }
     public void attack(Unit u) { /* 내용 생략 */ }
 }
 
-class Unit {
+class Unit1 {
     int currentHP;	// 유닛의 체력
     int x;			// 유닛의 위치(x좌표)
     int y;			// 유닛의 위치(y좌표)
